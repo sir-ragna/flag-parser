e7d16c4deb2d3a21ac7075c9891b1aece03a88c8
ragna <sir.ragna@gmail.com>
2021-10-27 20:57:57 +0200

    rewrite of example.c to use the new API

--- example.c
+++ example.c
@@ -4,32 +4,64 @@
 
 int main(int argc, const char *argv[])
 {
-    int *amount = flg_int_arg("-a", "--amount", 1, "Specify the amount");
-    char **dns = flg_string_arg("-d", "--dns", "1.1.1.1", "Specify the target DNS server");
-    bool *keep = flg_bool_arg("-k", "--keep", "Keep temp files");
-    
-    /* Optionally define the non-option args */
-    flg_define_rest_collection("FILE", 0, "Files to process");
+    /* Example of an integer */
+    int *amount = flg_int_arg(
+        "-a",                /* short flag format */
+        "--amount",          /* long flag format */
+        1,                   /* default value */
+        "Specify the amount" /* Help string */
+    );
 
+    /* Example of a boolean argument, also called a switch
+     * The defaul value of switches is always false */
+    bool *keep = flg_bool_arg(
+        "-k",             /* short flag format */
+        "--keep",         /* long flag format */
+        "Keep temp files" /* help string */
+    );
+
+    /* Example a string argument */
+    char **dns = flg_string_arg(
+        "-d",                           /* short flag format */
+        "--dns",                        /* long flag format */
+        "1.1.1.1",                      /* default value */
+        "Specify the target DNS server" /* help string */
+    );
+    
+    /* You can optionally define a 'rest collection'.
+     * This will help the documentation string to name what you
+     * expect to be present after the [OPTIONS]. */
+    flg_define_rest_collection(
+        "FILE",            /* Name of the collection.
+                            * ./example [OPTIONS] [FILE]... */
+        0,                 /* minimum amount of args required */
+        "Files to process" /* Help string */
+    );
+
+    /* flg_parse_flags will populate the pointers 
+     * It will also return an offset you can use to know when the 
+     * [OPTIONS] and where the 'rest collection' starts. */
     int offset = flg_parse_flags(argc, argv);
 
-    /* process the rest collection. (The arguments after the OPTIONS) */
+    /* Optional demonstration on how to deal with the non-flag strings
+     * arguments. The _offset_ variable allows us to iterate over them.
+     */
     int i;
     for (i = offset; i < argc; i++)
     {
         printf("Rest ARG: %i \"%s\"\n", i - offset, argv[i]);
     }
     
+    /* Demonstration on flag usage */
     if (*keep)
-        printf("Keep is enabled ");
-    printf("DNS server \"%s\", amount: %d\n", *dns, *amount);
+        puts("Keep is enabled");
+    else
+        puts("Keep is not enabled");
+    
+    printf("DNS server: \"%s\"\n", *dns);
+    printf("Amount: %d\n", *amount);
 
-    /* You still need to clean up your own pointers 
-     * The reason I am not putting this in a separate clean up func
-     * is because then you would NEED to call that function to free
-     * this memory. This seems non-obvious. This way the API remains
-     * minimal. You define the args, you call parseFlags and afterwards
-     * you are responsible for the cleanup yourself. */
+    /* Clean-up */
     free(amount);
     free(keep);
     free(*dns);
