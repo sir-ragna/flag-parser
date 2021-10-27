fd88bc990f69e47f7ee04c899c702aff725f1211
ragna <sir.ragna@gmail.com>
2021-10-27 17:30:38 +0200

    add test function to test for memory leaks

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -4,6 +4,22 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
+check_memory_leaks()
+{
+    TARGET=$1
+    shift
+    TMPFILE=/tmp/TARGET-valgrind-out.tmp
+    valgrind --leak-check=full --errors-for-leak-kinds=all --log-file="${TMPFILE}" --error-exitcode=33 "./$TARGET" $@ >/dev/null
+    RETCODE=$?
+    if [ "$?" -eq "33" ]; then # we have a memory issue
+        printf " ${RED}[FAIL]${NC} ${TARGET} <- Failed memory test\n"
+        cat "${TMPFILE}"
+    else # we don't have a memory issue (we don't care about other ret codes)
+         # *\(o_O)/*
+        printf " ${GREEN}[PASS]${NC} ${TARGET} memtest\n"
+        #cat "${TMPFILE}"
+    fi
+}
 
 compile_and_assert()
 {
@@ -21,12 +37,12 @@ compile_and_assert()
     fi
 
     # Execute the target, hide the output
-    "./${TARGET}" $@ >/dev/null 2>/dev/null
+    "./${TARGET}" "$@" >/dev/null 2>/dev/null
     ERR="$?"
     rm "${TARGET}" # remove the target
 
     if [ "$ERR" -eq "$EXPECTED_CODE" ]; then
-        printf " ${GREEN}[PASS]${NC} ${TARGET}\n"
+        printf " ${GREEN}[PASS]${NC} ${TARGET} retcode\n"
     else
         printf " ${RED}[FAIL]${NC} ${TARGET} <- "
         printf "${TARGET} expected ${EXPECTED_CODE} exit code got ${ERR}\n"
@@ -39,3 +55,9 @@ compile_and_assert 4 null_define_bool
 compile_and_assert 4 null_define_string
 compile_and_assert 4 null_define_int
 compile_and_assert 1 repeated_parse_flags
+compile_and_assert 0 define_rest_collection
+
+make all >/dev/null
+check_memory_leaks define_rest_collection test
+make clean >/dev/null
+
